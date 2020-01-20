098a47e82a40e2c4f0389f04e9749e32de1ff415
Justin Force <justin.force@gmail.com>
2020-01-19 16:43:08 -0800

    Persist tip jar total

--- src/TipJar.js
+++ src/TipJar.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+
+const TOTAL = "total";
 
 const TipJar = () => {
-  const [total, setTotal] = useState(0);
-  const tip = amount => () => setTotal(amount + total);
-  const empty = () => setTotal(0);
+  const initialTotal = (localStorage.getItem(TOTAL) || 0) * 1;
+  const [total, setTotal] = useState(initialTotal);
+  const updateTotal = useCallback(
+    amount => {
+      setTotal(amount);
+      localStorage.setItem(TOTAL, amount);
+    },
+    [setTotal]
+  );
+  const tip = useCallback(amount => () => updateTotal(amount + total), [
+    total,
+    updateTotal
+  ]);
+  const empty = useCallback(() => updateTotal(0), [updateTotal]);
   return (
     <>
       <h2>Tip Jar</h2>
